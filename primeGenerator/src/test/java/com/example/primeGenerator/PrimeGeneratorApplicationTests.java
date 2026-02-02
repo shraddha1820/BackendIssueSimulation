17d95e20173946f1c5056191d9a0f3e2d03de186
shraddha1820 <shraddha.singh30@gmail.com>
2026-02-02 08:37:53 +0530

    intial spring setup

---
+++ primeGenerator/src/test/java/com/example/primeGenerator/PrimeGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

41db1e696c07a21cd9cee9f99ae7c3b327fbc868
shraddha1820 <shraddha.singh30@gmail.com>
2026-02-07 11:14:58 +0530

    intial commit

---
+++ ticket-seller/src/main/java/com/example/ticket_seller/TicketSellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSellerApplication.class, args);
+	}
+
+}
---
+++ ticket-seller/src/test/java/com/example/ticket_seller/TicketSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticket_seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketSellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
